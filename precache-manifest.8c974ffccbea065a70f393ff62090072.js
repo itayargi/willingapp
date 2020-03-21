8417582b3b497e986eaf8afccb856803aa9f5a08
itayargi <itayhaza@gmail.com>
2020-03-21 16:43:40 +0200

    Updates

---
+++ precache-manifest.8c974ffccbea065a70f393ff62090072.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c349eff29106283a2b52063b9109237d",
+    "url": "/itayargi/willingapp.git/index.html"
+  },
+  {
+    "revision": "25c6f0ad45be7ec3f7fb",
+    "url": "/itayargi/willingapp.git/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c71098347a8356a0de9b",
+    "url": "/itayargi/willingapp.git/static/css/main.77862792.chunk.css"
+  },
+  {
+    "revision": "25c6f0ad45be7ec3f7fb",
+    "url": "/itayargi/willingapp.git/static/js/2.2dfbbd1a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/itayargi/willingapp.git/static/js/2.2dfbbd1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71098347a8356a0de9b",
+    "url": "/itayargi/willingapp.git/static/js/main.6ea01cb8.chunk.js"
+  },
+  {
+    "revision": "35ca77ab2bfdffb8934d",
+    "url": "/itayargi/willingapp.git/static/js/runtime-main.91565520.js"
+  },
+  {
+    "revision": "1fcdc4eec564011b719ff88438256717",
+    "url": "/itayargi/willingapp.git/static/media/willing.1fcdc4ee.png"
+  }
+]);
